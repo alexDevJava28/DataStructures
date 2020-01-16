6cb7f4bbb10c126434e97b90e03fbd19249a437c
Oleksii Khodakivskyi <oleksii.khodakivskyi@check24.de>
2020-01-16 13:43:52 +0100

    added TypeScript implementation

---
+++ Stack/JavaScript/Stack.ts
@@ -0,0 +1,70 @@
+'use strict';
+
+export class Stack {
+
+    stack: Array<number>;
+    top: number;
+    capacity: number;
+
+    constructor(capacity: number) {
+        this.stack = [];
+        this.top = 0;
+        this.capacity = capacity;
+    }
+
+    length(): number {
+        return this.top;
+    }
+
+    isEmpty(): boolean {
+        return this.top === 0;
+    }
+
+    isFull(): boolean {
+        return this.top >= this.capacity;
+    }
+
+    push(item: number): void {
+
+        if (this.isFull()) {
+            console.log('The Stack is overflow. It is impossible to add new element.');
+            return;
+        }
+
+        this.stack[this.top++] = item;
+
+    }
+
+    pop(): number {
+
+        if (this.isEmpty()) {
+            console.log('The Stack is empty. It is impossible to remove element.');
+            return 0;
+        }
+
+        --this.top;
+        return this.stack.pop();
+    }
+
+    peek(): number {
+
+        if (this.isEmpty()) {
+            console.log('The Stack is empty. It is impossible to peek element.');
+            return 0;
+        }
+
+        return this.stack[this.top - 1];
+    }
+
+    print(): void {
+
+        let index = 0;
+
+        while (index < this.top) {
+            console.log(index + ': ' + this.stack[index]);
+            index++;
+        }
+
+    }
+
+}
---
+++ Stack/JavaScript/test.ts
@@ -0,0 +1,38 @@
+'use strict';
+
+import { Stack } from "./Stack";
+
+let capacity = 5;
+let stack = new Stack(capacity);
+
+console.log('Stack size at the beginning - ', stack.length());
+
+console.log('Pop element from empty Stack...');
+stack.pop();
+
+for(let i = capacity; i > 0; i--) {
+    //get random number from 1 to 10000
+    let randomNumber = Math.floor(Math.random() * 10000) + 1;
+    console.log('Inserting ', randomNumber);
+    stack.push(randomNumber);
+}
+
+stack.print();
+
+console.log('Pop element from the Stack...', stack.pop());
+
+stack.print();
+
+console.log('Inserting ', 10001);
+stack.push(10001);
+
+stack.print();
+console.log('Stack size in the end - ', stack.length());
+
+console.log('Inserting ', 10002);
+
+try {
+    stack.push(10002);
+} catch(error) {
+    console.error(error);
+}
